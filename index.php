da94addb20a89f7e5c484b9b34e1be16f6c342fc
michael lucas <michael16klucas@gmail.com>
2022-11-14 23:56:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,46 @@
 <!DOCTYPE html>
 <html>
-<head>
+   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="./painel/favicon.svg" type="image/svg+xml">
-    <title>Login</title>
-<link rel="stylesheet" type="text/css" href="./assets/style.css">
-</head>
-<body>
-    <div class="box-login">
+    <title>Tela de Login</title>
+    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+   </head>
 
-    <div class="title">
-        <h1>Login</h1>
+   <body>
+
+    <div class="box-itens">
+        <div class="titulo">
+            <h1>Login</h1>
+        </div>
+
+        <form action="#">
+
+            <div class="itens">
+                <label>Nome ou E-mail</label>
+                <input type="email" placeholder="Digite seu nome ou email aqui" required/>
+            </div>
+
+            <div class="itens">
+                <label>Senha</label>
+                <input type="password" placeholder="*****************" required/>
+            </div>
+
+            <div class="remenber">
+                <input type="checkbox">
+                <label>Mantenha-me Conectado</label>
+            </div>
+
+            <div class="itens">
+                <button class="submit" type="submit">Entrar</button>
+                <a href="#">Esqueci minha senha</a>
+            </div>
+
+        </form>
+        <small>Ainda não possui conta? <a href="#">Cadastre-se aqui!</a></small>
+
     </div>
+   </body>
 
-    <form action="">
 
-        <div class="itens">
-            <label>Nome ou e-mail:</label>
-            <input type="text" placeholder="Digite aqui seu nome ou e-mail">
-        </div>
-
-        <div class="itens">
-            <label>Senha:</label>
-            <input type="password" placeholder="*********">
-        </div>
-
-        <div class="remenber">
-            <input type="checkbox">
-            <label for="save">Mantenha-me conectado</label>
-        </div>
-
-        <div class="itens">
-            <button class="submit">Entrar</button>
-            <a href="#">Esqueci minha senha</a>
-        </div>
-
-    </form>
-    <small>Ainda não possui uma conta? <br><a href="#">Cadastre-se aqui!</a></small>
-</div>
-</body>
 </html>
